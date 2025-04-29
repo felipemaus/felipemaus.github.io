14d92cca05bef1c781d3f9a703c92574824d7f9d
Felipe Flores Maus <felipe.maus@paipe.co>
2025-04-28 23:56:21 -0300

    Updates

--- src/assets/mySelf/index.ts
+++
@@ -1,6 +0,0 @@
-import Walter from "./walter.jpg";
-import TernoSelf from "./ternoself.jpg";
- 
-export {
-      TernoSelf, Walter
-}
--- src/models/NavLinks.ts
+++
@@ -1,7 +0,0 @@
-export const NavLinks = [
-    { name: "Home", id: "home" },
-    { name: "About", id: "about" },
-    { name: "Projects", id: "projects" },
-    { name: "Skills", id: "skills" },
-    { name: "Contact", id: "contact" },
-  ];
